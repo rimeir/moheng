26b1806e8076639116ced0358a6169ecaad8a680
Haon <88240193+msung99@users.noreply.github.com>
2024-07-22 19:10:51 +0900

    #1 feat: 초기 개발환경 구축

---
+++ backend/src/main/java/mohang/MohangApplication.java
@@ -0,0 +1,13 @@
+package mohang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohangApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/mohang/MohangApplicationTests.java
@@ -0,0 +1,13 @@
+package mohang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
