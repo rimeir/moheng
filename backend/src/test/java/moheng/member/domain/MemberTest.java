60edffdc5e11fbe1c69b351534f613f64a74b986
Haon <88240193+msung99@users.noreply.github.com>
2024-07-25 13:35:07 +0900

    #5 test: 회원을 정상적으로 생성하는지 테스트

--- backend/src/test/java/moheng/member/domain/MemberTest.java
+++ backend/src/test/java/moheng/member/domain/MemberTest.java
@@ -1,4 +1,17 @@
 package moheng.member.domain;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class MemberTest {
+
+    @DisplayName("회원을 생성한다.")
+    @Test
+    public void 회원을_생성한다() {
+        // given, when, then
+        Assertions.assertDoesNotThrow(() ->
+                new Member("msung6924@naver.com", "msung99",
+                        "profile_img_url", SocialType.KAKAO));
+    }
 }
