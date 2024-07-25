8fe4c79c4f01397da8f28f62063494ab8af513f5
Haon <88240193+msung99@users.noreply.github.com>
2024-07-25 15:36:25 +0900

    #5 test: 구글 소셜로그인 회원 생성 테스트

--- backend/src/test/java/moheng/member/domain/MemberTest.java
+++ backend/src/test/java/moheng/member/domain/MemberTest.java
@@ -6,12 +6,21 @@ import org.junit.jupiter.api.Test;
 
 public class MemberTest {
 
-    @DisplayName("회원을 생성한다.")
+    @DisplayName("카카오 소셜로그인 회원을 생성한다.")
     @Test
-    public void 회원을_생성한다() {
+    public void 카카오_소셜로그인_회원을_생성한다() {
         // given, when, then
         Assertions.assertDoesNotThrow(() ->
                 new Member("msung6924@naver.com", "msung99",
                         "profile_img_url", SocialType.KAKAO));
+    }
+
+    @DisplayName("구글 소셜로그인 회원을 생성한다.")
+    @Test
+    public void 구글_소셜로그인_회원을_생성한다() {
+        // given, when, then
+        Assertions.assertDoesNotThrow(() ->
+                new Member("msung6924@naver.com", "msung99",
+                        "profile_img_url", SocialType.GOOGLE));
     }
 }
