f4044bc5193831ae316c082a4a5f71c6b3945180
Haon <88240193+msung99@users.noreply.github.com>
2024-07-25 13:06:03 +0900

    #5 feat: ControllerAdvice 활성화 및 Slf4j Logging 추가

--- backend/src/main/java/moheng/global/error/ControllerAdvice.java
+++ backend/src/main/java/moheng/global/error/ControllerAdvice.java
@@ -1,4 +1,23 @@
 package moheng.global.error;
 
+import moheng.global.error.dto.ErrorResponse;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestControllerAdvice
 public class ControllerAdvice {
+    private static final Logger logger = LoggerFactory.getLogger(ControllerAdvice.class);
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleRunTimeException(Exception e) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
 }
