116bfebe3de0af0c1739e16c8b42539f7a1af055
Haon <88240193+msung99@users.noreply.github.com>
2024-07-25 16:00:45 +0900

    #5 test: 닉네임 형식이 올바르지 않다면 예외가 발생하는지 테스트

--- backend/src/test/java/moheng/member/domain/MemberTest.java
+++ backend/src/test/java/moheng/member/domain/MemberTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import moheng.member.exception.InvalidEmailFormatException;
+import moheng.member.exception.InvalidNicknameFormatException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,9 +33,19 @@ public class MemberTest {
     @DisplayName("이메일 형식이 올바르지 않다면 예외가 발생한다.")
     @Test
     public void 이메일_형식이_올바르지_않다면_예외가_발생한다() {
-        // given & when & then
+        // given, when, then
         assertThatThrownBy(() -> new Member("msung6924naver.com", "msung99",
                 "profile_img_url", SocialType.KAKAO))
                 .isInstanceOf(InvalidEmailFormatException.class);
+    }
+
+    @DisplayName("닉네임 형식이 올바르지 않다면 예외가 발생한다.")
+    @Test
+    public void 닉네임_형식이_올바르지_않다면_예외가_발생한다() {
+        // given, when, then
+        assertThatThrownBy(() -> new Member("msung6924@naver.com",
+                "qweiqnweiqnweiqwieqniweiqweiqwneiqnweqwienwqeqieqnweiqwneiqwenqweiqnweiqweqweqweqweinqwneiqwei",
+                "profile_img_url", SocialType.KAKAO))
+                .isInstanceOf(InvalidNicknameFormatException.class);
     }
 }
