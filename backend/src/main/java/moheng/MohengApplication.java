ab774aaf7454d2c18a1f8afae9318de45ad06e26
Haon <88240193+msung99@users.noreply.github.com>
2024-07-25 12:43:21 +0900

    #5 chore: 애플리케이션 네이밍 Moheng 으로 변경

--- backend/src/main/java/moheng/MohengApplication.java
+++ backend/src/main/java/moheng/MohengApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MohangApplication {
+public class MohengApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MohangApplication.class, args);
+		SpringApplication.run(MohengApplication.class, args);
 	}
 
 }
