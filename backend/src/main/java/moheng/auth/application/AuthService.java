56f69aa08d2b17c7bbd010a9d1c3dce0f9bda023
Haon <88240193+msung99@users.noreply.github.com>
2024-07-26 21:38:49 +0900

    #12 feat: 회원 관리를 위한 MemberService 클래스 생성

--- backend/src/main/java/moheng/auth/application/AuthService.java
+++ backend/src/main/java/moheng/auth/application/AuthService.java
@@ -1,21 +1,25 @@
 package moheng.auth.application;
 
-import jakarta.transaction.Transactional;
 import moheng.auth.domain.OAuthClient;
 import moheng.auth.domain.OAuthMember;
 import moheng.auth.domain.OAuthUriProvider;
 import moheng.auth.dto.TokenResponse;
-import org.springframework.beans.factory.annotation.Value;
+import moheng.member.application.MemberService;
+import moheng.member.domain.Member;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AuthService {
     private final OAuthUriProvider oAuthUriProvider;
     private final OAuthClient oAuthClient;
+    private final MemberService memberService;
 
-    public AuthService(final OAuthUriProvider oAuthUriProvider, final OAuthClient oAuthClient) {
+    public AuthService(final OAuthUriProvider oAuthUriProvider, final OAuthClient oAuthClient,
+                       final MemberService memberService) {
         this.oAuthUriProvider = oAuthUriProvider;
         this.oAuthClient = oAuthClient;
+        this.memberService = memberService;
     }
 
     @Transactional
@@ -24,6 +28,7 @@ public class AuthService {
         return new TokenResponse("access_token");
     }
 
+    @Transactional(readOnly = true)
     public String generateUri() {
         return oAuthUriProvider.generateUri();
     }
