e5fac00508cc0f2a83e6be20db438dc862112a8c
Haon <88240193+msung99@users.noreply.github.com>
2024-07-26 19:48:45 +0900

    #12 test: 카카오 로그인을 위한 링크를 정상적으로 생성하는지 테스트

--- backend/src/test/java/moheng/auth/application/AuthServiceTest.java
+++ backend/src/test/java/moheng/auth/application/AuthServiceTest.java
@@ -1,4 +1,27 @@
 package moheng.auth.application;
 
-public class AuthServiceTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@SpringBootTest
+class AuthServiceTest {
+
+    @Autowired
+    private AuthService authService;
+
+    @DisplayName("카카오 로그인을 위한 링크를 생성한다.")
+    @Test
+    void 카카오_로그인을_위한_링크를_생성한다() {
+        // given
+        String link = authService.generateLink();
+
+        // when & then
+        assertThat(link).isNotEmpty();
+    }
 }
