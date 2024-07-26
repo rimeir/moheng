5c4390784edbdfa0f839ae5cc973cbe2d2258bd7
Haon <88240193+msung99@users.noreply.github.com>
2024-07-26 22:25:31 +0900

    #12 feat: AuthController 의 Authorization URI 생성 기능 구현

--- backend/src/main/java/moheng/auth/presentation/AuthController.java
+++ backend/src/main/java/moheng/auth/presentation/AuthController.java
@@ -1,4 +1,25 @@
 package moheng.auth.presentation;
 
+import moheng.auth.application.AuthService;
+import moheng.auth.dto.OAuthUriResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @GetMapping("/{provider}/link")
+    public ResponseEntity<OAuthUriResponse> generateUri(@PathVariable final String provider) {
+        return ResponseEntity.ok(new OAuthUriResponse(authService.generateUri()));
+    }
+
 }
