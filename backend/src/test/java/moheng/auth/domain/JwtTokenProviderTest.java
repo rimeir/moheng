42211cfe88a0b7b04add783a520d7278e5d20aaa
Haon <88240193+msung99@users.noreply.github.com>
2024-07-27 11:30:14 +0900

    #26 test: JWT Token 을 정상적으로 생성하는지 테스트

--- backend/src/test/java/moheng/auth/domain/JwtTokenProviderTest.java
+++ backend/src/test/java/moheng/auth/domain/JwtTokenProviderTest.java
@@ -1,4 +1,27 @@
 package moheng.auth.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class JwtTokenProviderTest {
+    private static final String SECRET_KEY = "secret_secret_secret_secret_secret_secret_secret_";
+    private static final int EXPIRE_TIME = 3600;
+    private final JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(SECRET_KEY, EXPIRE_TIME);
+
+    @DisplayName("JWT 토큰을 생성한다.")
+    @Test
+    void JWT_토큰을_생성한다() {
+        // given
+        String payload = "hello";
+
+        // when
+        String actual = jwtTokenProvider.createToken(payload);
+
+        // then
+        assertThat(actual.split("\\.")).hasSize(3);
+    }
 }
