58c318a2e3e1609d4b59f0cca0744bfb46c81ebe
Haon <88240193+msung99@users.noreply.github.com>
2024-07-27 11:36:51 +0900

    #26 test: 만료된 토큰을 전달받으면 예외가 발생하는지 테스트

--- backend/src/test/java/moheng/auth/domain/JwtTokenProviderTest.java
+++ backend/src/test/java/moheng/auth/domain/JwtTokenProviderTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import moheng.auth.exception.InvalidTokenException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -37,5 +38,17 @@ public class JwtTokenProviderTest {
 
         // then
         assertThat(actual).isEqualTo(exptected);
+    }
+
+    @DisplayName("만료된 토큰을 전달받으면 예외가 발생한다.")
+    @Test
+    void 만료된_토큰을_전달받으면_예외가_발생한다() {
+        // given
+        JwtTokenProvider expiredJwtTokenProvider = new JwtTokenProvider(SECRET_KEY, 0);
+        String expiredToken = expiredJwtTokenProvider.createToken("payload");
+
+        // when & then
+        assertThatThrownBy(() -> jwtTokenProvider.validateToken(expiredToken))
+                .isInstanceOf(InvalidTokenException.class);
     }
 }
