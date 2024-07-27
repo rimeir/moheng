229718c88c9ceb3c289903fd77d1f561eb671cc8
Haon <88240193+msung99@users.noreply.github.com>
2024-07-27 11:46:51 +0900

    #26 test: 이메일을 통해 회원을 정상적으로 찾는지 테스트

---
+++ backend/src/test/java/moheng/member/domain/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package moheng.member.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import moheng.config.TestConfig;
+import moheng.member.domain.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TestConfig.class)
+public class MemberRepositoryTest {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @DisplayName("이메일을 통해 회원을 찾는다.")
+    @Test
+    void 이메일을_통해_회원을_찾는다() {
+        // given
+        String email = "msung6924@naver.com";
+        String nickname = "msung99";
+        String profile_image_url = "https://msung99";
+        Member member = new Member(email, nickname, profile_image_url, SocialType.KAKAO);
+        Member savedMember = memberRepository.save(member);
+
+        // when
+        Member foundMember = memberRepository.findByEmail(email).get();
+
+        // then
+        assertThat(savedMember.getId()).isEqualTo(foundMember.getId());
+    }
+}
