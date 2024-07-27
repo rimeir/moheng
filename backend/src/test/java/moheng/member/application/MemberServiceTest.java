21c1edf0ee15778ca479fc9f983b03ef667eb858
Haon <88240193+msung99@users.noreply.github.com>
2024-07-27 11:56:34 +0900

    #26 test: 소셜 로그인을 시도한 회원 정보를 정상적으로 저장하는지 테스트

--- backend/src/test/java/moheng/member/application/MemberServiceTest.java
+++ backend/src/test/java/moheng/member/application/MemberServiceTest.java
@@ -1,4 +1,29 @@
 package moheng.member.application;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import moheng.member.domain.Member;
+import moheng.member.domain.SocialType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class MemberServiceTest {
+    @Autowired
+    private MemberService memberService;
+
+    @DisplayName("소셜 로그인을 시도한 회원을 저장한다.")
+    @Test
+    void 소셜_로그인을_시도한_회원을_저장한다() {
+        // given
+        Member member = new Member("msung6924@naver.com", "msung99",
+                "profile_image_url", SocialType.KAKAO);
+
+        // when, then
+        assertDoesNotThrow(() -> memberService.save(member));
+    }
 }
